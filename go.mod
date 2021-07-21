8c61aea0d671d079efb8c0b21b900ceaf4f5f88a
Lix3nn53 <lixakyildiz@gmail.com>
2021-07-21 22:48:08 +0300

    quick start gin-gonic

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lix3nn53/goa-golang
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
