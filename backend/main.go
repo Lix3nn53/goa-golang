a77cc4111cfbddfb82bf75c4399d8a18d645fc71
Lix3nn53 <lixakyildiz@gmail.com>
2021-07-22 22:14:50 +0300

    Add test for main.go

--- backend/main.go
+++ backend/main.go
@@ -6,14 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r := setupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
