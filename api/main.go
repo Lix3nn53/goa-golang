fa32a34bcbd6d26b1f3063694cd9a50ca1384691
Lix3nn53 <lixakyildiz@gmail.com>
2021-07-23 23:47:31 +0300

    Create v1 router group

--- api/main.go
+++ api/main.go
@@ -15,25 +15,31 @@ func DummyMiddleware(c *gin.Context) {
 	c.Next()
 }
 
+func Ping(c *gin.Context) {
+	c.String(200, "pong")
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	// Simple group: v1
+	v1 := r.Group("/v1")
 
-	r.GET("/test", DummyMiddleware, func(c *gin.Context) {
-		c.String(200, "test successful")
-	})
+	v1.Use(DummyMiddleware)
+	{
+		v1.StaticFile("/favicon.ico", "./public/favicon.ico")
+		v1.GET("/ping", Ping)
 
-	// handler for documentation
-	opts := middleware.RedocOpts{SpecURL: "/swagger.yml"}
-	sh := middleware.Redoc(opts, nil)
+		v1.GET("/test", DummyMiddleware, func(c *gin.Context) {
+			c.String(200, "test successful")
+		})
 
-	r.GET("/docs", gin.WrapH(sh))
-
-	r.StaticFile("/swagger.yml", "./public/swagger.yml")
-	r.StaticFile("/favicon.ico", "./public/favicon.ico")
+		// handler for documentation
+		opts := middleware.RedocOpts{BasePath: "/v1", SpecURL: "/v1/swagger.yml"}
+		sh := middleware.Redoc(opts, nil)
+		v1.GET("/docs", gin.WrapH(sh))
+		v1.StaticFile("/swagger.yml", "./public/swagger.yml")
+	}
 
 	return r
 }
