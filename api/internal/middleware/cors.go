9d3819799f8298f758afe79f1f3c808d2be3d144
Lix3nn53 <lixakyildiz@gmail.com>
2021-08-20 23:36:03 +0300

    Get cors url drom env file

--- api/internal/middleware/cors.go
+++ api/internal/middleware/cors.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,7 +21,7 @@ func NewCorsMiddleware() CorsMiddlewareInterface {
 //Handler ...
 func (cm corsMiddleware) Handler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv(("CORS_URL")))
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
